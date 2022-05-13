c4aafcf273dd7201dc1f5b3c9902d7d326e431cc
flamminger <101122248+flamminger@users.noreply.github.com>
2022-05-13 16:04:37 +0200

    added links to images in datatable

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@
 //     console.log('Connected to Postkarten.');
 // });
 //
-// const sql = "SELECT * FROM 'Abfrage Haupttabelle';";
+// const sql = "SELECT * FROM 'Abfrage Haupttabelle' LIMIT 100 OFFSET 100;";
 //
 // function get_view(){
 //     return new Promise((resolve) => {
@@ -34,41 +34,22 @@
 // get_entries()
 
 
-function generate_picture_name() {
-    let arr = [];
-    let i;
-    for(i = 1; i<204; i++){
-        let base_sub10 = 'PK0000_V';
-        let base_sub100 = 'PK000_V';
-        let base_sub1000 = 'PK00_V';
-        if (i<10) {
-            let picture = base_sub10+i;
-            arr.push(picture);
-        } else if (i < 100) {
-            let picture = base_sub100+i;
-            arr.push(picture);
-        } else {
-            let picture = base_sub1000+i;
-            arr.push(picture)
-        }
-
-    }
-    return arr
-}
-
-// generate_picture_name()
-
-
-//datatable | jquery
-$(document).ready( function () {
+// datatable | jquery
+$(document).ready(function () {
     $('#postcard_table').DataTable({
-        'ajax' : '../data/postcards.json',
-        'columns' : [
-            {'data' : 'Schiff', defaultContent: ""},
-            {'data' : 'Klasse', defaultContent: ""},
-            {'data' : 'Beschreibung', defaultContent: ""}
+        'ajax': '../data/postcards.json',
+        'columnDefs': [
+            { className: "entry-head-desc", "targets": [ 3 ] }
+        ],
+        'columns': [
+            {'data': 'Schiff', defaultContent: ""},
+            {'data': 'Klasse', defaultContent: ""},
+            {'data': 'Beschreibung', defaultContent: ""},
+            {'data': 'Bild', "render": function(data) {
+                return '<a href="../assets/'+ data + '_V.jpg" target="_blank" class="text-muted entry-btn">Go</a>';
+                }}
         ]
     });
-} );
+});
 
 
