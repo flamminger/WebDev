25d4a0cedffa277b0cfb6db5d65f654aee532ecd
flamminger <101122248+flamminger@users.noreply.github.com>
2022-06-05 16:37:15 +0200

    fixed datatable arrows

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,42 +1,9 @@
-// mit NodeJS SQLite ausgeben
-// Refactor Schachtelkürzel zu Schachtel
-// Output  als json in data/postcards.json gespeichert
-//
-// const sqlite3 = require('sqlite3').verbose();
-// // const db = new sqlite3.Database('../data/database/Postkarten.sqlite');
-//
-// let db = new sqlite3.Database('../data/Postkarten.db', (err) => {
-//     if (err) {
-//         console.error(err.message);
-//     }
-//     console.log('Connected to Postkarten.');
-// });
-//
-// const sql = "SELECT * FROM 'Abfrage Haupttabelle';";
-//
-// function get_view(){
-//     return new Promise((resolve) => {
-//         db.all(sql,[],(err,rows)=>{
-//             if(err){
-//                 return console.error(err.message);
-//             }
-//
-//             resolve(rows);
-//         });
-//     });
-// }
-//
-// async function get_entries() {
-//     const entries = await get_view();
-//     console.log(entries)
-// }
-//
-// get_entries()
 
 
 // datatable | jquery
 $(document).ready(function () {
     $('#postcard_table').DataTable({
+        'responsive': true,
         'ajax': '/WebDev/data/postcards.json',
         'columnDefs': [
             {className: "entry-head-desc", "targets": [3]}
